61572a69ea444e4d637e8af73af2dedc76b5f33f
Pawan Gulati <pawangulat225@gmail.com>
2022-01-14 04:25:59 +0530

    Initial commit

---
+++ src/main/java/com/springboot/project/ScheduleManagementSystem/ScheduleManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.project.ScheduleManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManagementSystemApplication.class, args);
+	}
+
+}
